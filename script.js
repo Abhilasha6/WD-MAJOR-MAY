4c391a8f083c06bb52ce9b83fceca8651888c526
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-07-08 19:52:53 +0530

    added icons

--- script.js
+++ script.js
@@ -72,17 +72,17 @@ window.addEventListener("load", () => {
           climate.textContent = main;
           tempvalue.textContent = Math.round(feels_like - 273);
           if (id < 300 && id > 200) {
-            tempicon.src = "./icons/thunderstorm.svg";
+            tempicon.src = "./assets/rain.png";
           } else if (id < 400 && id > 300) {
-            tempicon.src = "./icons/cloud-solid.svg";
+            tempicon.src = "./assets/cloudy.png";
           } else if (id < 600 && id > 500) {
-            tempicon.src = "./icons/rain.svg";
+            tempicon.src = "./assets/onlyrain.jpg";
           } else if (id < 700 && id > 600) {
-            tempicon.src = "./icons/snow.svg";
+            tempicon.src = "./assets/snow.png";
           } else if (id < 800 && id > 700) {
-            tempicon.src = "./icons/clouds.svg";
+            tempicon.src = "./assets/manyclouds.png";
           } else if (id == 800) {
-            tempicon.src = "./icons/clouds-and-sun.svg";
+            tempicon.src = "./assets/normal.png";
           }
 
           console.log(data);
